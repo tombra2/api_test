e60276a50dc2ef015aa9163f76f9186aaacbd0a3
tom_bra_surface <thomas.brandner@edu.htl-klu.at>
2024-06-28 16:48:05 +0200

    winrate ok

--- main.js
+++ main.js
@@ -1,16 +1,47 @@
+var balance = 0;
+var risk=0;
+var reward=0;
+var riskDollar=0;
+var rewardDollar=0;
+var riskReward=0;
 
-function addStudent() {
 
-    var name = document.getElementById("name").value; 
-    var id = document.getElementById("id").value;
-    var nc = document.getElementById("nc").value;
-
-var student = {
-    "Name": name,
-    "Matrikelnummer": id,
-    "Notendurschnitt": nc,
+function inital () {
+  balance = parseInt(document.getElementById('balance').value);
+  risk = parseFloat(document.getElementById('risk').value);
+  reward = parseFloat(document.getElementById('reward').value);
+  
 }
-var students = [];    
-students.push(student);
-console.log(students);
-}
+
+
+
+riskDollar = balance * risk/100;
+rewardDollar = balance * reward/100;
+riskReward = riskDollar/rewardDollar;
+console.log(riskDollar, rewardDollar, riskReward);
+  
+
+// Calculate winrate and lossrate and update
+balance
+
+
+var score = 0;
+var win=0;
+var loss=0;
+
+document.getElementById('win').addEventListener('click', function calculateWinrate() {
+  win ++;
+  score ++;
+  balance += rewardDollar;
+  console.log(win/score*100 , "$", balance);
+});
+
+
+document.getElementById('loss').addEventListener('click', function calculateLossrate() {
+  loss ++;
+  score ++;
+  
+  balance -= riskDollar;  
+  console.log(win/score*100, "$", balance);
+  
+}); 
