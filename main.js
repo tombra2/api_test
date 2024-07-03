dc1480ddecaf4a4981cec49d990cfaf30d2c0c31
tom_bra <thomas.brandner@edu.htl-klu.at>
2024-07-03 19:18:42 +0200

    no changes added to commit (use "git add" and/or "git commit -a")

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ var rewardDollar=0;
 var riskReward=0;
 var commision=0;
 var sumCommision=0;
+var startBalance=0;
+var pnl=0;
+var pnl_2=0;
+var sumWin=0;
+var sumLoss=0;  
 
 
 function inital() {
@@ -15,9 +20,10 @@ function inital() {
   risk = parseFloat(document.getElementById('risk').value);
   reward = parseFloat(document.getElementById('reward').value);
   commision = parseFloat(document.getElementById('commision').value);
+  startBalance=parseFloat(document.getElementById('balance').value);
   
   var balance_display=document.getElementById('balance_display')
-balance_display.innerText="Balance: "+balance.toFixed(2)+"$";
+  balance_display.innerText="Balance: "+balance.toFixed(1)+"$";
 
   
 }
@@ -26,16 +32,33 @@ balance_display.innerText="Balance: "+balance.toFixed(2)+"$";
 
 function update() {
 var winrate_display=document.getElementById('winrate_display')
-winrate_display.innerText="Winrate: "+winrate.toFixed(2)+"%"	
+winrate_display.innerText="Winrate: "+winrate.toFixed(1)+"%"	
 
 var balance_display=document.getElementById('balance_display')
-balance_display.innerText="Balance: "+balance.toFixed(2)+"$";
+balance_display.innerText="Balance: "+balance.toFixed(1)+"$";
 
 var score_display=document.getElementById('score_display')
-score_display.innerText="Score: "+score.toFixed(0);
+score_display.innerText="Total Trades: "+score.toFixed(0);
 
 var commision_display=document.getElementById('commision_display')
-commision_display.innerText="Commision: "+sumCommision.toFixed(2)+"$";
+commision_display.innerText="Commision: "+sumCommision.toFixed(1)+"$";
+
+var winloss_display=document.getElementById('winloss_display')
+winloss_display.innerText="Win: "+win.toFixed(0)+" Loss: "+loss.toFixed(0);
+
+var pnl_display=document.getElementById('pnl_display')
+pnl = balance-startBalance+sumCommision;
+pnl_display.innerText="PnL: "+pnl.toFixed(1)+"$";
+
+var pnl_2_display=document.getElementById('pnl_2_display')
+pnl_2 = balance*100/startBalance-100;
+pnl_2_display.innerText="PnL: "+pnl_2.toFixed(1)+"%";
+
+
+var pf_display=document.getElementById('pf_display');
+var profitFaktor
+
+
 }
 
 
@@ -54,9 +77,11 @@ document.getElementById('win').addEventListener('click', function calculateWinra
   sumCommision= sumCommision+commision;
 
   rewardDollar = balance * reward/100;
-  balance += rewardDollar;
+  balance += rewardDollar-sumCommision;
   winrate = win/score*100;
-  console.log(winrate , "$", balance);
+  
+  
+
  
 update();
 
@@ -70,9 +95,9 @@ document.getElementById('loss').addEventListener('click', function calculateLoss
   sumCommision= sumCommision+commision
   riskDollar = balance * risk/100;
   
-  balance -= riskDollar;  
+  balance -= riskDollar-sumCommision;  
   winrate = win/score*100;
-  console.log(winrate, "$", balance);
+ 
   
 update ()
 
@@ -87,6 +112,10 @@ balance = 0;
 winrate = 0;
 score = 0;
 sumCommision = 0;
+
+win=0;
+loss=0;
+
 update();
   
 });
